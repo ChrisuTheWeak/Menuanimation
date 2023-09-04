c563f2e1cfc571840c9cb9605c5c762d9012d105
Christian <christio@metropolia.fi>
2023-09-04 13:59:37 +0300

    hamburgr fixed not wronk

--- components/HamburgerIcon.js
+++ components/HamburgerIcon.js
@@ -1,11 +1,30 @@
+import LottieView from 'lottie-react-native';
 import PropTypes from 'prop-types';
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
+import Hamburger from '../Lottie/Hamburger.json';
+import HamburgerReverse from '../Lottie/HamburgerReverse.json';
+import { useEffect, useState } from 'react';
+import { useDrawerStatus } from '@react-navigation/drawer';
 
 const HamburgerIcon = ({ navigation }) => {
+  const [anim, setAnim] = useState (Hamburger);
+  const isDrawerOpen = useDrawerStatus() !== 'open';
+
+  const pressHandler = () =>{
+    navigation.toggleDrawer();
+  }
+useEffect(() => {
+  isDrawerOpen ? setAnim(HamburgerReverse) : setAnim(Hamburger);
+},[isDrawerOpen]);
   return (
-    <View>
-      <Text>HamburgerIcon</Text>
-    </View>
+    <TouchableOpacity onPress={pressHandler}>
+      <LottieView 
+      source={anim} 
+      style={{height:40, width:40, marginLeft:10 }}
+      autoPlay={true}
+      loop={false}
+      />
+    </TouchableOpacity>
   );
 };
 
